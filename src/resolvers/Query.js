8a03ac8451f43f38a102893a7f1864d4c58ca29e
happysenior <happinessalex231@gmail.com>
2019-12-10 12:38:04 +0900

    filtering and pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links()
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.fileter },
+      { url_contains: args.filter }
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
+  return links
 }
 
 module.exports = {
